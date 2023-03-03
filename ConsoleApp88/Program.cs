9f0e9e49a9f00dea5d8454df09cb1faf2fb2ef2a
unknown <mrpmorris@gmail.com>
2023-03-03 21:44:36 +0000

    Initial commit

---
+++ ConsoleApp88/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ConsoleApp88
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello, World!");
+		}
+	}
+
+	public class Person
+	{
+		[MinLength(32)]
+		public string Name { get; set; }
+	}
+}
